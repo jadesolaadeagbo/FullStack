1ba3f3cb21181b7c87b6cc00153afa3d13bdef86
Jadesola Adeagbo <jadesolaadeagbo@gmail.com>
2025-06-14 12:59:25 +0100

    Added none to samesite

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -43,7 +43,7 @@ app.use(
     cookie: {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
-      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      sameSite: "none",
       maxAge: 1000 * 60 * 60 * 24,
     },
   })
